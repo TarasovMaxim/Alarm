c3b6688d0ce4744c9f4c18fa4d2d944b5fee7290
TarasovMaxim <tarasovma96@gmail.com>
2025-10-15 20:04:29 +0300

    init

--- Assets/Scripts/House.cs
+++ Assets/Scripts/House.cs
@@ -1,11 +1,10 @@
-using System;
-using System.Net.NetworkInformation;
 using UnityEngine;
 
 public class House : MonoBehaviour
 {
     [SerializeField] private AlarmRing _alarmRing;
     [SerializeField] private TriggerZone _triggerZone;
+
     private void OnEnable()
     {
         _triggerZone.RobberEntered += _alarmRing.Ring;
